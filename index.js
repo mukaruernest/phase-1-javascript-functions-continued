15bd9452e3a2f20b1b32bb00fa2b7b643ab19ca4
mukaruernest <mukaruernesto@gmail.com>
2023-01-12 12:02:11 +0300

    functions: continued submition

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork()
+
+const wrapAdjective = function (adjective = "*") {
+
+    return function (meaning = "special") {
+        return `You are ${adjective}${meaning}${adjective}!`
+    }
+}
+wrapAdjective("||")("a dedicated programmer")
+
